e71a59b606ae2967a3444c915c1436440e967cf1
MarlonTU <131878309+MarlonTU@users.noreply.github.com>
2023-05-16 12:35:25 +0200

    Add files via upload
    src

---
+++ java/com/example/harzstempelroutenapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.harzstempelroutenapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.harzstempelroutenapp.R
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Konfigurieren der Bottom Navigation View
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_map, R.id.navigation_filter, R.id.navigation_profile))
+        navView.setupWithNavController(navController)
+    }
+}
